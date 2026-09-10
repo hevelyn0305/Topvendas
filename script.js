a5a713cdb35774f12273a28d571d990048f48c63
Aluno <aluno.formoso@ifto.local>
2026-09-10 09:04:54 -0300

    mensagem

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMenu = document.querySelector("#btn-menu");
+const menu = document.querySelector(".nav");
+const logoh2 = document.querySelector(".logo-h2");
+
+botaoMenu.addEventListener("click", function () {
+    menu.classList.toggle("ativo");
+    logo.classList.toggle("ativo");
+});
